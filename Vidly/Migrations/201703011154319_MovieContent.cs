97525715d496273b88a7ec9bf06489df347512a7
Dario Vranjković <vranja33@gmail.com>
2017-03-01 13:26:32 +0100

    Add movies controller, view, model

---
+++ Vidly/Migrations/201703011154319_MovieContent.cs
@@ -0,0 +1,20 @@
+namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MovieContent : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("INSERT INTO MOVIES (Name, GenreId, DateAdded, ReleaseDate, NumberInStock) VALUES ('Hangover', 1, '1.1.2001','1.1.2000', 50 )");
+            Sql("INSERT INTO MOVIES (Name, GenreId, DateAdded, ReleaseDate, NumberInStock) VALUES ('Die Hard', 2, '1.1.2002','1.1.2001', 15 )");
+            Sql("INSERT INTO MOVIES (Name, GenreId, DateAdded, ReleaseDate, NumberInStock) VALUES ('Titanic', 3, '1.1.2003','1.1.2002', 150 )");
+            Sql("INSERT INTO MOVIES (Name, GenreId, DateAdded, ReleaseDate, NumberInStock) VALUES ('Mask', 4, '1.1.2004','1.1.2003', 5 )");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
